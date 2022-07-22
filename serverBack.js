04ab6836c38cd32ed0fefa6110876ab73e40134e
rodrigolopez14 <rodrigo.lopez@14agency.com>
2022-07-22 10:40:20 +0200

    New try 2

--- serverBack.js
+++ serverBack.js
@@ -6,35 +6,35 @@ const axios = require('axios')
 //app.use(express.static(path.join(__dirname, './build')));
 app.use(cors());
 
-app.get('/*', async function (req, res) {
+// app.get('/*', async function (req, res) {
 
-  //res.sendFile(path.join(__dirname, './build', 'index.html'));
-  let xmls = `<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-          <soap:Body>
-            <Divide xmlns="http://tempuri.org/">
-              <intA>5</intA>
-              <intB>5</intB>
-            </Divide>
-          </soap:Body>
-        </soap:Envelope>`
+//   //res.sendFile(path.join(__dirname, './build', 'index.html'));
+//   let xmls = `<?xml version="1.0" encoding="utf-8"?>
+//         <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+//           <soap:Body>
+//             <Divide xmlns="http://tempuri.org/">
+//               <intA>5</intA>
+//               <intB>5</intB>
+//             </Divide>
+//           </soap:Body>
+//         </soap:Envelope>`
 
-       const result = await axios.post('http://www.dneonline.com/calculator.asmx',
-           xmls,
-           {headers:
-             {'Access-Control-Allow-Origin': '*',
-             'Content-Type': 'text/xml',
-            }
-           }).then(res=>{
+//        const result = await axios.post('http://www.dneonline.com/calculator.asmx',
+//            xmls,
+//            {headers:
+//              {'Access-Control-Allow-Origin': '*',
+//              'Content-Type': 'text/xml',
+//             }
+//            }).then(res=>{
              
-             return res
+//              return res
              
-           }).catch(err=>{console.log(err)});
-           console.log(result.data)
-           res.send(result.data)
+//            }).catch(err=>{console.log(err)});
+//            console.log(result.data)
+//            res.send(result.data)
 
-});
-app.post('/new', async function (req, res) {
+// });
+app.get('/', async function (req, res) {
  
 
 
