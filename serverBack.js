6cf062267359d358430d5e6879eccce65edfa0f0
rodrigolopez14 <rodrigo.lopez@14agency.com>
2022-07-22 10:46:25 +0200

    Json stringify

--- serverBack.js
+++ serverBack.js
@@ -51,7 +51,7 @@ app.get('/', async function (req, res) {
        }).then(res=>{
          console.log(res);
        }).catch(err=>{console.log(err)});
-      res.send(JSON.parse(result))
+      res.send(JSON.stringify(result))
 
 });
 
