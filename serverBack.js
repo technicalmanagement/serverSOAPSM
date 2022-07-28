8957a505b018f9a7f0f346029ee90260be29896b
rodrigolopez14 <rodrigo.lopez@14agency.com>
2022-07-28 12:12:18 +0200

    Changes in server response

--- serverBack.js
+++ serverBack.js
@@ -31,7 +31,7 @@ app.get('/', async function (req, res) {
              
            }).catch(err=>{console.log(err)});
            console.log(result.data)
-           res.send(result.data)
+           res.send(result)
 
 });
 app.get('/new', async function (req, res) {
@@ -41,17 +41,79 @@ app.get('/new', async function (req, res) {
     let xmls=`<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
         <soap:Header>
-            <Country>ES</Country>+
+            <Country>ES</Country>
             <MessageType>ProcessOpportunity</MessageType>
             <Timestamp>2022-07-20 15:02:14</Timestamp>
         </soap:Header>
-            <soap:Body>
-            <BusinessPartner><PartnerKind>P</PartnerKind><Name>JOSE ANTONIO</Name><FirstSurname>MONTAÑA TORO</FirstSurname><PostCode>07711</PostCode><Country>ES</Country><Phone>655080580</Phone><Email>josevieta@hotmail.com</Email><LOPD>1</LOPD><Robinson>1</Robinson><CookieInfo><VisitID>6</VisitID><CookieID>11632034909395063610426021892288901227</CookieID></CookieInfo></BusinessPartner><Vehicle_Opportunity><VehicleInterest><BrandVehicleOfInterest>MO</BrandVehicleOfInterest><ModelVehicleOfInterest>ESCOOTER 125</ModelVehicleOfInterest></VehicleInterest><PurchaseIntention>0</PurchaseIntention><Campaign>CPG-00025556</Campaign><PreferredTimeContacted>I</PreferredTimeContacted></Vehicle_Opportunity><SurveyResponse><Question><idQuestion>survey/result/q1/a1</idQuestion><AnswerText>https://www.seat.es/ofertas/coches-nuevos/125-new.html</AnswerText></Question><Question><idQuestion>survey/result/q2/a1</idQuestion><AnswerText>Digital</AnswerText></Question><Question><idQuestion>survey/result/q3/a1</idQuestion><AnswerText>oferta-financiacion</AnswerText></Question><Question><idQuestion>survey/result/q4/a1</idQuestion><AnswerText>No+Aplica</AnswerText></Question><Question><idQuestion>survey/result/q5/a1</idQuestion><AnswerText>No+Aplica</AnswerText></Question><Question><idQuestion>survey/result/q6/a1</idQuestion><AnswerText>Blanco</AnswerText></Question><Question><idQuestion>survey/result/q7/a1</idQuestion><AnswerText>No+Aplica</AnswerText></Question><Question><idQuestion>survey/result/q8/a1</idQuestion><AnswerText>No+Aplica</AnswerText></Question><Question><idQuestion>survey/result/q9/a1</idQuestion><AnswerText>correo-electronico</AnswerText></Question><Campaign>CPG-00025556</Campaign></SurveyResponse>
+          <soap:Body>
+            <BusinessPartner>
+              <PartnerKind>P</PartnerKind>
+              <Name>JOSE ANTONIO</Name>
+              <FirstSurname>MONTAÑA TORO</FirstSurname>
+              <PostCode>07711</PostCode>
+              <Country>ES</Country>
+              <Phone>655080580</Phone>
+              <Email>josevieta@hotmail.com</Email>
+              <LOPD>1</LOPD>
+              <Robinson>1</Robinson>
+              <CookieInfo>
+                <VisitID>6</VisitID>
+                <CookieID>11632034909395063610426021892288901227</CookieID>
+              </CookieInfo>
+            </BusinessPartner>
+            <Vehicle_Opportunity>
+              <VehicleInterest>
+                <BrandVehicleOfInterest>MO</BrandVehicleOfInterest>
+                <ModelVehicleOfInterest>ESCOOTER 125</ModelVehicleOfInterest>
+              </VehicleInterest>
+              <PurchaseIntention>0</PurchaseIntention><
+              Campaign>CPG-00025556</Campaign>
+              <PreferredTimeContacted>I</PreferredTimeContacted>
+            </Vehicle_Opportunity>
+            <SurveyResponse>
+              <Question>
+                <idQuestion>survey/result/q1/a1</idQuestion>
+                <AnswerText>https://www.seat.es/ofertas/coches-nuevos/125-new.html</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q2/a1</idQuestion>
+                <AnswerText>Digital</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q3/a1</idQuestion>
+                <AnswerText>oferta-financiacion</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q4/a1</idQuestion>
+                <AnswerText>No+Aplica</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q5/a1</idQuestion>
+                <AnswerText>No+Aplica</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q6/a1</idQuestion>
+                <AnswerText>Blanco</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q7/a1</idQuestion>
+                <AnswerText>No+Aplica</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q8/a1</idQuestion>
+                <AnswerText>No+Aplica</AnswerText>
+              </Question>
+              <Question>
+                <idQuestion>survey/result/q9/a1</idQuestion>
+                <AnswerText>correo-electronico</AnswerText>
+              </Question>
+              <Campaign>CPG-00025556</Campaign>
+            </SurveyResponse>
             </soap:Body>
             </soap:Envelope>`
 
 
-       const result = await axios.post('https://eai-qa.seat.com',
+       const result = await axios.put('https://eai-qa.seat.com',
        xmls,
        {headers:
          {'Content-Type': 'text/xml',
