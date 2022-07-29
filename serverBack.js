ada40df3ba97f32616993e93a9b3c4669073130e
rodrigolopez14 <rodrigo.lopez@14agency.com>
2022-07-29 13:27:19 +0200

    New try

--- serverBack.js
+++ serverBack.js
@@ -119,7 +119,6 @@ app.get('/new', async function (req, res) {
        xmls,
        {headers:
          {'Content-Type': 'text/xml',
-         'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
         'Access-Control-Allow-Origin': '*'}
        }).then(response=>{
          console.log(response)
